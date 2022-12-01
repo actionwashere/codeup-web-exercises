58c4c57486ad30bab3b7b650ec08f477fa1a86f5
aharownjackson <aharownmjackson@gmail.com>
2022-12-01 11:12:48 -0600

    finished 2nd problem

--- js/for-loops.js
+++ js/for-loops.js
@@ -1,12 +1,12 @@
 // create function for multiplying numbers 1 - 10
 
-showMultiplicationTable(1);
+// showMultiplicationTable(1);
 // showMultiplicationTable(2);
 // showMultiplicationTable(3);
 // showMultiplicationTable(4);
 // showMultiplicationTable(5);
 // showMultiplicationTable(6);
-// showMultiplicationTable(7);
+showMultiplicationTable(7);
 // showMultiplicationTable(8);
 // showMultiplicationTable(9);
 // showMultiplicationTable(10);
@@ -18,4 +18,32 @@ function showMultiplicationTable(input) {
         console.log(`${input} * ${i} = ${(input * i)}`);
     }
 }
+
+
+getRandomInt(20, 200);
+
+
+function getRandomInt(min, max) {
+    for(let i = 0; i < 10; i++) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        let num = (Math.floor(Math.random() * (max - min) + min));
+        console.log(`The Number ${num} is ${isEven(num)}`);
+
+    }
+}
+
+function isEven(num) {
+    return (num % 2 == 0) ? "even" : "odd";
+}
+
+
+// function isEven(num) {
+//     if (num % 2 == 0) {
+//         return "even";
+//     } else {
+//         return "odd";
+//     }
+// }
+
 
