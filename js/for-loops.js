45ffd6318ce20e67d6ecd40e52b0eda18de354d0
aharownjackson <aharownmjackson@gmail.com>
2022-12-01 10:07:44 -0600

    started loop exercises

---
+++ js/for-loops.js
@@ -0,0 +1,29 @@
+// create function for multiplying numbers 1 - 10
+
+showMultiplicationTable(1);
+showMultiplicationTable(2);
+showMultiplicationTable(3);
+showMultiplicationTable(4);
+showMultiplicationTable(5);
+showMultiplicationTable(6);
+showMultiplicationTable(7);
+showMultiplicationTable(8);
+showMultiplicationTable(9);
+showMultiplicationTable(10);
+
+
+
+function showMultiplicationTable(input) {
+    console.log(`${input} * 1 = ${input * 1}`);
+    console.log(`${input} * 2 = ${input * 2}`);
+    console.log(`${input} * 3 = ${input * 3}`);
+    console.log(`${input} * 4 = ${input * 4}`);
+    console.log(`${input} * 5 = ${input * 5}`);
+    console.log(`${input} * 6 = ${input * 6}`);
+    console.log(`${input} * 7 = ${input * 7}`);
+    console.log(`${input} * 8 = ${input * 8}`);
+    console.log(`${input} * 9 = ${input * 9}`);
+    console.log(`${input} * 10 = ${input * 10}`);
+}
+
+
