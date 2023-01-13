72733cd84abba366a77ad6e68b38602627d392f2
aharownjackson <aharownmjackson@gmail.com>
2023-01-13 16:02:40 -0600

    finished jquery exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,6 +1,17 @@
 "use strict"
 
+let borders = $('.codeup').css("border", "solid 1px  red");
 
-let contents = $('#welcome-message').html();
-alert(contents);
+let fontSize = $('li').css("font-size", "20px");
+
+// let h1 = $('h1').css('background-color', 'yellow');
+// let p = $('p').css('background-color', 'yellow');
+// let li = $('li').css('background-color', 'yellow');
+
+let highlight = $('h1, p, li').css('background-color', 'yellow');
+
+let message = $('#welcome-message').html()
+alert(message);
+
+
 
