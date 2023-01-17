02762729c7e04177feeabe07293750920f4f72c5
aharownjackson <aharownmjackson@gmail.com>
2023-01-17 15:10:52 -0600

    finished konami exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,17 +1,34 @@
 "use strict"
 
-let borders = $('.codeup').css("border", "solid 1px  red");
+// let borders = $('.codeup').css("border", "solid 1px  red");
+//
+// let fontSize = $('li').css("font-size", "20px");
+//
+// // let h1 = $('h1').css('background-color', 'yellow');
+// // let p = $('p').css('background-color', 'yellow');
+// // let li = $('li').css('background-color', 'yellow');
+//
+// let highlight = $('h1, p, li').css('background-color', 'yellow');
+//
+// let message = $('#welcome-message').html()
+// alert(message);
 
-let fontSize = $('li').css("font-size", "20px");
+let $h1btn = $('#welcome-message');
 
-// let h1 = $('h1').css('background-color', 'yellow');
-// let p = $('p').css('background-color', 'yellow');
-// let li = $('li').css('background-color', 'yellow');
+$h1btn.click(function () {
+    $h1btn.css('background-color', 'yellow');
+});
 
-let highlight = $('h1, p, li').css('background-color', 'yellow');
+let $allPara = $('p');
 
-let message = $('#welcome-message').html()
-alert(message);
+$allPara.dblclick(function(){
+    $allPara.css('font-size', '18px')
+});
 
+let $allLiItems = $('li');
 
-
+$allLiItems.hover(function(){
+    $allLiItems.css('color', 'red');
+}, function(){
+    $allLiItems.css('color', 'black');
+});
