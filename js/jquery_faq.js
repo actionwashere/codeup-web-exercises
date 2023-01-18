fb27db0a78b47d77bed84f55b9e64cf6716bf316
aharownjackson <aharownmjackson@gmail.com>
2023-01-18 16:11:43 -0600

    finished jquery_faq

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
 
     // ***** dt click listener ****
-    $(`dt`).on(`click`, function () {
+    $(`dt`).on(`click`, function(){
         $(this).toggleClass(`highlight`).next().toggleClass(`invisible`);
+    });
+
+    // ***** make it yellow btn *****
+    $(`#btnYellow`).on(`click`, function(){
+        $(`ul`).each(function(index){
+            $(this).children('li').last().toggleClass(`yellowBg`);
+        });
+    });
+
+    // ***** h3 bold list items *****
+    $(`h3`).on(`click`, function(){
+        $(this).next().children(`li`).toggleClass(`bolder`);
+    });
+
+    // ***** list item turns first li blue *****
+    $(`li`).on(`click`, function(){
+        $(this).parent().each(function(){
+            $(this).children(`li`).first().toggleClass(`makeBlue`);
+        });
     });
 
 });
