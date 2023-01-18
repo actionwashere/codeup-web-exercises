60e9618da2d182e6bc3f7065a96b9f488d0bf21e
aharownjackson <aharownmjackson@gmail.com>
2023-01-18 12:07:35 -0600

    added document ready function for jquery

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,4 +1,9 @@
+$(document).ready(function () {
 
-$(`dt`).on(`click`, function(){
-    $(this).toggleClass(`highlight`).next().toggleClass(`invisible`);
+    // ***** dt click listener ****
+    $(`dt`).on(`click`, function () {
+        $(this).toggleClass(`highlight`).next().toggleClass(`invisible`);
+    });
+
 });
+
