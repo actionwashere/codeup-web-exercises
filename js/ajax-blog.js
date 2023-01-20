4bfd8604e14dd640abb780633e6b4f4f10b66d17
aharownjackson <aharownmjackson@gmail.com>
2023-01-20 12:31:18 -0600

    finished blog

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
             html += '    <article class="d-flex flex-column align-items-center">\n' +
                 '        <h3 id="title" class="mt-3">' + posts[i].title + '</h3>\n' +
                 '            <p id="content" class="mt-3">' + posts[i].content + '</p>\n' +
-                '            <p id="categories">' + posts[i].categories + '</p>\n' +
+                '            <p id="categories" class="align-self-start mt-3"">' + posts[i].categories + '</p>\n' +
                 '            <p id="date">' + posts[i].date + '</p>\n' +
                 '    </article>';
         }
@@ -26,7 +26,6 @@ $(document).ready(function(){
 
     function updatePage() {
         // make request to orders.json
-        console.log('Update orders');
         $.get('./data/blog.json').done(function(posts) {
             let postsHTML = createHTML(posts); // create HTML string
             $('#posts').html(postsHTML); // update the DOM with the new HTML
@@ -38,6 +37,12 @@ $(document).ready(function(){
     }
 
     updatePage();
+
+    $(`#add-btn`).on(`click`, function(){
+        let input = $(`#newBlog`);
+        input.val('');
+        alert(`WE DONT GIVE A FXXK!`);
+    });
 
 });
 
