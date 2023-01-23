89f16eb339624e7e244663fe77f75286f5e827dd
aharownjackson <aharownmjackson@gmail.com>
2023-01-23 14:08:58 -0600

    finished weather app layout

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+    mapboxgl.accessToken = keys.mapbox;
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 10,
+        center: [-98.4916, 29.4252]
+    });
+
 
 
 
